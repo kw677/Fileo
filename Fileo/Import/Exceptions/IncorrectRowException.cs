94ad15d2b16a35fa1772ec93c1bbe4b669fbba29
kw677 <fileo.even.simpler@gmail.com>
2017-05-26 22:16:03 +0200

    Fileo

---
+++ Fileo/Import/Exceptions/IncorrectRowException.cs
@@ -0,0 +1,18 @@
+﻿using Fileo.Common;
+using System;
+
+namespace Fileo.Import.Exceptions
+{
+    internal class IncorrectRowException : ApplicationException
+    {
+        private readonly int _rowIndex;
+
+        public IncorrectRowException(int rowIndex)
+        {
+            _rowIndex = rowIndex;
+        }
+
+
+        public override string Message => string.Format(ErrorMessage.IncorrectRow, _rowIndex);
+    }
+}
---
+++ Fileo/Import/Exceptions/IncorrectValueException.cs
@@ -0,0 +1,22 @@
+﻿using Fileo.Common;
+using System;
+
+namespace Fileo.Import.Exceptions
+{
+    internal class IncorrectValueException : ApplicationException
+    {
+        private readonly int _rowIndex;
+        private readonly int _columnIndex;
+        private readonly string _columnName;
+
+        public IncorrectValueException(int rowIndex, int columnIndex, string columnName)
+        {
+            _rowIndex = rowIndex;
+            _columnIndex = columnIndex;
+            _columnName = columnName;
+        }
+        
+
+        public override string Message => string.Format(ErrorMessage.IncorrectValue, _rowIndex, _columnIndex, _columnName);
+    }
+}
